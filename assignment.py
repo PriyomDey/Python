6307a41ce98ee132f74534ab4658b514ea74ebfa
PriyomDey <157955824+PriyomDey@users.noreply.github.com>
2024-01-30 11:51:46 +0530

    assignment

---
+++ assignment.py
@@ -0,0 +1,13 @@
+#Write a python program taking input from the user 
+#i.e. name,age,gender(single character),weight 
+#and display the output of the same.
+
+name = input("Enter your name:- ")
+age = input("Enter your age:- ")
+gender = input("Enter your age(M/F):- ")
+weight = input("Enter your weight(in kgs):- ")
+
+print(name)
+print(age)
+print(gender)
+print(weight)
