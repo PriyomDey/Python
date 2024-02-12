cc651fe395c9e4098049d0f75e20ecb59546ac1d
PriyomDey <157955824+PriyomDey@users.noreply.github.com>
2024-02-12 22:52:06 +0530

    NaturalReverse

---
+++ Naturalreverse.py
@@ -0,0 +1,13 @@
+#write a program to print all natural nos. in reverse.
+
+# n=int(input("Enter the total no. of natural nos. to be reversed: "))
+
+# i=n
+# while i>=1:
+#  i=i-1
+    
+#  print(i)
+
+n=1
+for i in range(n,0,-1):
+    print(i)
